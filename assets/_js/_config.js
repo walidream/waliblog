cfcbef220bd0ed9f487b2e3da0a77fa71ce2bed3
walidream <walidream@163.com>
2017-12-01 11:42:43 +0800

    添加webpack打包和添加新帖子

---
+++ assets/_js/_config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/11/26.
+ */
+
+ 
+const PATH = {
+	"node_modules": '../../_wp/node_modules',
+	"jq": '../../_wp/node_modules/jquery/dist/jquery.js',
+	"gitment": '../../_wp/node_modules/gitment/dist/gitment.js'
+	
+}
+
+const CONFIG = {
+	"wlbUrl":{
+		"getCommet": 'https://api.github.com/repos/walidream/waliblog/issues'
+	}
+}
+
+const GLVARIABLE = {
+	"ghComment":[]
+}
+
+export {PATH, CONFIG, GLVARIABLE}
+
